f579b59901e770f40246a887d2ad870a75a8b91c
Ripal Barot <ripplejb@hotmail.com>
2019-10-30 04:06:18 -0500

    Removed unnecessary files/

--- DropboxToastmastersApp/ClientApp/src/app/app.module.ts
+++ DropboxToastmastersApp/ClientApp/src/app/app.module.ts
@@ -7,24 +7,19 @@ import {CookieService} from 'ngx-cookie-service';
 
 import {AppComponent} from './app.component';
 import {NavMenuComponent} from './nav-menu/nav-menu.component';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
 import {AccountService} from "./services/account/account.service";
 import {DropBoxSignInGuard} from "./guards/dropboxsignin/drop-box-sign-in.guard";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    LoginComponent
+    NavMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'},
       {
         path: 'dropboxSignIn',
         canActivate: [DropBoxSignInGuard],
